ee7a4e7768e413c040ee1ae2569692eea4e2484d
Armin <salehi@umich.edu>
2025-02-03 01:12:55 +0000

    Submiting AceofSpades HW - Feb 2nd 2025

--- exercise_2/exercise_2_solution.js
+++ exercise_2/exercise_2_solution.js
@@ -1,1 +1,72 @@
 // Copy and paste js here
+function createDeck() {
+  fetch("https://deckofcardsapi.com/api/deck/new/shuffle")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // Extract the deck ID from the response
+      const deckId = data.deck_id;
+      // Print the deck ID
+      console.log(`Deck ID: ${deckId}`);
+      deck.textContent = data.deck_id;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function drawHand() {
+  let deckId = document.getElementById("deck").textContent;
+  let handCount = document.getElementById("handCount").value;
+  fetch(
+    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${handCount}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      var hand = document.getElementById("hand");
+      hand.innerHTML = "";
+      data.cards.forEach((card) => {
+        var image = document.createElement("img");
+        image.src = card.image;
+        hand.appendChild(image);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function drawAceOfSpades() {
+  // Your work goes here
+  let deckId = document.getElementById("deck").textContent;
+  let rem_handCount = 52 - document.getElementById("handCount").value;
+
+  fetch(
+    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${rem_handCount}`
+  )
+    .then((response) => response.json())
+    .then((data2) => {
+      // Find the ace of spades in the drawn cards
+      console.log("data2 is: \n");
+      console.log(data2);
+      const aceOfSpades = data2.cards.find(
+        (card) => card.value === "ACE" && card.suit === "SPADES"
+      );
+      var aceofSpades = document.getElementById("aceofSpades");
+
+      if (aceOfSpades) {
+        const imageUrl = aceOfSpades.image;
+
+        var image = document.createElement("img");
+        image.src = imageUrl;
+        aceofSpades.appendChild(image);
+      } else {
+        console.log("Ace of spades not found in the drawn cards.");
+      }
+    })
+    .catch((error) => {
+      // Handle any errors
+      console.error("Error:", error);
+    });
+}
